3c464ca43c1480f8a00ef853dabde4edd387dff4
Ken Qiu <kenin.qiu@gmail.com>
2022-08-18 00:17:34 -0400

    added version

--- src/app/modules/dashboard/deshboard.routing.module.ts
+++ src/app/modules/dashboard/deshboard.routing.module.ts
@@ -1,11 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { DashboardComponent } from './dashboard.component';
+import { VersionComponent } from './version/version.component';
 
 const routes: Routes = [
   {
     path: '',
     component: DashboardComponent
+  },
+  {
+    path: 'version',
+    component: VersionComponent
   },
 ];
 
