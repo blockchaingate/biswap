1c4e558f4a11282b6180334b5f95aa12ad3a8662
Paul Liu <paullby@gmail.com>
2023-07-17 06:54:03 -0400

    update

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -8,7 +8,8 @@ export const environment = {
 
 
   // smartConractAdressRouter:  "0xd99bfcbfad77f57b5ed20286c24ad71785d73993", - old router, can't charge fee
-  smartConractAdressRouter:  "0xb05c29010751d3aec15bc98602fad16c92fe25eb",    //New router with FEE
+  // smartConractAdressRouter:  "0xb05c29010751d3aec15bc98602fad16c92fe25eb",    //New router with FEE - this one charges wrong fee.
+  smartConractAdressRouter:  "0xf2fd85a831d25859c4d0b8f493bb7bfd5c27c0b3",    //New router with FEE
   smartConractAdressProxy:   "0xe4d478abfc9592188a3c71468d3e837628922121",
   smartConractAdressFactory: "0x8aa15747311e39f31d89bd3082d20e1463a311c8",
   smartConractStaking: '',
