5d082c647ddc7958349643bdceff60ea7b01d203
jacksun0124 <42154731+jacksun0124@users.noreply.github.com>
2024-02-21 15:11:24 -0500

    update UI

--- src/app/modules/swap/swap.component.ts
+++ src/app/modules/swap/swap.component.ts
@@ -1,4 +1,4 @@
-import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { Component, ElementRef, OnInit, Renderer2, ViewChild } from '@angular/core';
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { ActivatedRoute, Router } from '@angular/router';
 import BigNumber from 'bignumber.js';
@@ -15,8 +15,9 @@ import { Web3Service } from 'src/app/services/web3.service';
 import { environment } from 'src/environments/environment';
 import { TokenListComponent } from '../shared/tokenList/tokenList.component';
 import { SettingsComponent } from '../settings/settings.component';
-import {MatDialog, MatDialogRef, MAT_DIALOG_DATA} from '@angular/material/dialog';
+import { MatDialog, MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { AlertComponent } from '../shared/alert/alert.component';
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-swap',
@@ -37,6 +38,11 @@ export class SwapComponent implements OnInit {
   token1Element!: ElementRef;
   @ViewChild('token2')
   token2Element!: ElementRef;
+
+  @ViewChild('animatedElement', { static: true }) 
+  animatedElement!: ElementRef;
+
+
   isFistToken!: boolean;
 
   _firstToken!: Coin;
@@ -49,7 +55,7 @@ export class SwapComponent implements OnInit {
   public set firstToken(coin: Coin) {
     this._firstToken = coin;
     const id = coin.id;
-    if(this.account && id) {
+    if (this.account && id) {
       this.kanbanService.getTokenBalance(this.account, id).subscribe(
         (balance: any) => {
           this.firstCoinBalance = balance;
@@ -58,7 +64,7 @@ export class SwapComponent implements OnInit {
     }
 
   }
- 
+
   public get secondToken(): Coin {
     return this._secondToken;
   }
@@ -66,7 +72,7 @@ export class SwapComponent implements OnInit {
   public set secondToken(coin: Coin) {
     this._secondToken = coin;
     const id = coin.id;
-    if(this.account && id) {
+    if (this.account && id) {
       this.kanbanService.getTokenBalance(this.account, id).subscribe(
         (balance: any) => {
           this.secondCoinBalance = balance;
@@ -88,9 +94,9 @@ export class SwapComponent implements OnInit {
 
   public set account(newAccount: string) {
     this._account = newAccount;
-    
-    if(newAccount) {
-      if(this.firstToken && this.firstToken.id) {
+
+    if (newAccount) {
+      if (this.firstToken && this.firstToken.id) {
         this.kanbanService.getTokenBalance(newAccount, this.firstToken.id).subscribe(
           (balance: any) => {
             this.firstCoinBalance = balance;
@@ -98,7 +104,7 @@ export class SwapComponent implements OnInit {
         );
       }
 
-      if(this.secondToken && this.secondToken.id) {
+      if (this.secondToken && this.secondToken.id) {
         this.kanbanService.getTokenBalance(newAccount, this.secondToken.id).subscribe(
           (balance: any) => {
             this.secondCoinBalance = balance;
@@ -107,7 +113,7 @@ export class SwapComponent implements OnInit {
       }
     }
   }
-  
+
   tokenId!: string;
 
   firstCoinAmount!: number;
@@ -119,6 +125,12 @@ export class SwapComponent implements OnInit {
   secondCoinBalance!: number;
   firstCoinBalance!: number;
   txHashes: any = [];
+
+  t1 = "";
+  t2 = "";
+
+  t1ft = "";
+  t2ft = "";
 
   //isNewPair: boolean = false;
 
@@ -137,17 +149,20 @@ export class SwapComponent implements OnInit {
     private biswapServ: BiswapService,
     private currentRoute: ActivatedRoute,
     private apiService: ApiService,
-    private router: Router
-  ) {}
+    private router: Router,
+    private translate: TranslateService,
+    private renderer: Renderer2,
+    private elementRef: ElementRef
+  ) { }
 
   openSettings() {
     const dialogRef = this.dialog.open(SettingsComponent, {
       width: '250px',
-      data: {slippage: this.slippage, deadline: this.deadline},
+      data: { slippage: this.slippage, deadline: this.deadline },
     });
 
     dialogRef.afterClosed().subscribe(result => {
-      if(result) {
+      if (result) {
         this.slippage = result.slippage;
         this.deadline = result.deadline;
       }
@@ -156,13 +171,16 @@ export class SwapComponent implements OnInit {
   }
 
   ngOnInit() {
+    this.t1 = this.translate.instant('THE BEST CHOICE');
+    this.t2 = this.translate.instant('IS TO PROVIDE LIQUIDITY');
+
     this.firstToken = new Coin();
     this.secondToken = new Coin();
     this.secondCoinBalance = -1;
     this.firstCoinBalance = -1;
 
     this.account = this.walletService.account;
-    if(!this.account){
+    if (!this.account) {
       this.walletService.accountSubject.subscribe(
         account => {
           this.account = account;
@@ -170,32 +188,49 @@ export class SwapComponent implements OnInit {
       );
     }
     this.dataService.GettokenList.subscribe((x) => {
+      console.log('tokenList:', x);
       this.tokenList = x;
       // this._firstToken = this.tokenList.find(t => t.coinType == 131072) || new Coin();
     });
     this.checkUrlToken();
 
-    this.autorefresh = setInterval(() => {this.refresh()}, 1000);
+    this.autorefresh = setInterval(() => { this.refresh() }, 1000);
+
+    if (this.tokenList) {
+      //set default token to symbol = 'kbFAB'
+      this.tokenList.map((x) => {
+        if (x.symbol == 'kbFAB') {
+          this.firstToken = x;
+        }
+      });
+
+    }
   }
- 
- 
-  checkUrlToken(){
-    this.currentRoute.params.subscribe((x) =>{
+
+  animateElement() {
+    this.animatedElement.nativeElement.classList.add('show');
+  }
+
+
+
+
+  checkUrlToken() {
+    this.currentRoute.params.subscribe((x) => {
       var type = this.router.url.split("/")
       if (type[2] == "token") {
         let params: any = x;
-        if(params.tokenid) {
-          this.apiService.getTokenInfoFromId(params.tokenid).subscribe((res: any) =>{
+        if (params.tokenid) {
+          this.apiService.getTokenInfoFromId(params.tokenid).subscribe((res: any) => {
             let first = res["name"];
             this.firstToken = this.tokenList.find(x => x.symbol == first) || new Coin();
-          }) 
+          })
         }
- 
-      } else if(x.tokenid){
+
+      } else if (x.tokenid) {
         let params: any = x;
-        if(params.tokenid) {
-          this.apiService.getTokensInfoFromPair(params.tokenid).subscribe((res: any) =>{
-            if(res) {
+        if (params.tokenid) {
+          this.apiService.getTokensInfoFromPair(params.tokenid).subscribe((res: any) => {
+            if (res) {
               let first = res["token0Name"];
               let sescond = res["token1Name"];
               this.firstToken = this.tokenList.find(x => x.symbol == first) || new Coin();
@@ -205,7 +240,7 @@ export class SwapComponent implements OnInit {
         }
 
       }
-  });
+    });
   }
 
   async onKey(value: number, isFistToken: boolean) {
@@ -226,6 +261,35 @@ export class SwapComponent implements OnInit {
       this.token1Element.nativeElement.focus();
       this.token2Element.nativeElement.focus();
     }, 0);
+
+    //typing effect for t1ft and t2ft, data from t1 and t2
+    const speed = 70;
+    const t1ft = this.t1;
+    const t2ft = this.t2;
+    let i = 0;
+    let j = 0;
+    this.t1ft = "";
+    this.t2ft = "";
+    const typeWriter = () => {
+      if (i < t1ft.length + t2ft.length + 1) {
+        if (i < t1ft.length) {
+          this.t1ft += t1ft.charAt(i);
+        }
+        else if (i >= t1ft.length) {
+          this.t2ft += t2ft.charAt(j);
+          j++;
+        }
+
+        if (j == t2ft.length + 1) {
+          this.t2ft += "!";
+          this.animateElement();
+        }
+        i++;
+        setTimeout(typeWriter, speed);
+      }
+    }
+
+    typeWriter();
   }
 
   openDialog(errorMessage: String) {
@@ -233,7 +297,7 @@ export class SwapComponent implements OnInit {
   }
 
   getPair() {
-    if(!this.firstToken.id || !this.secondToken.id) {
+    if (!this.firstToken.id || !this.secondToken.id) {
       return;
     }
     var params = [this.firstToken.id, this.secondToken.id];
@@ -256,7 +320,7 @@ export class SwapComponent implements OnInit {
       })
       .afterClosed()
       .subscribe((x) => {
-        if ( x != undefined && x.isFirst) {
+        if (x != undefined && x.isFirst) {
           this.dataService.GetFirstToken.subscribe((data) => {
             this.firstToken = data;
           });
@@ -291,13 +355,13 @@ export class SwapComponent implements OnInit {
   }
 
   ngOnDestroy() {
-    if(this.autorefresh) {
+    if (this.autorefresh) {
       clearInterval(this.autorefresh);
     }
   }
 
   async setInputValues(isFirst: boolean) {
-    if(!this.tokenId || (this.tokenId == '0x0000000000000000000000000000000000000000')) {
+    if (!this.tokenId || (this.tokenId == '0x0000000000000000000000000000000000000000')) {
       return;
     }
     var abiHexa = this.web3Service.getReserves();
@@ -305,39 +369,38 @@ export class SwapComponent implements OnInit {
       .kanbanCall(this.tokenId, abiHexa)
       .subscribe((data1) => {
 
-          var param = ['uint112', 'uint112', 'uint32'];
-          let res: any = data1;
+        var param = ['uint112', 'uint112', 'uint32'];
+        let res: any = data1;
 
-          var value = this.web3Service.decodeabiHexs(res.data, param);
+        var value = this.web3Service.decodeabiHexs(res.data, param);
 
-          if (this.firstToken.id < this.secondToken.id) {
-            this.firstTokenReserve = new BigNumber(value[0]);
-            this.secondTokenReserve = new BigNumber(value[1]);
-          } else {
-            this.firstTokenReserve = new BigNumber(value[1]);
-            this.secondTokenReserve = new BigNumber(value[0]);
-          }
+        if (this.firstToken.id < this.secondToken.id) {
+          this.firstTokenReserve = new BigNumber(value[0]);
+          this.secondTokenReserve = new BigNumber(value[1]);
+        } else {
+          this.firstTokenReserve = new BigNumber(value[1]);
+          this.secondTokenReserve = new BigNumber(value[0]);
+        }
 
 
-          if (isFirst && this.firstCoinAmount) {
-            var amount: number = this.firstCoinAmount;
-            this.insufficientFund = (Number(amount) > Number(this.firstCoinBalance));
-            var reserve1: BigNumber = this.firstTokenReserve;
-            var reserve2: BigNumber = this.secondTokenReserve;
-            let value = '0x' + new BigNumber(amount)
-              .shiftedBy(this.firstToken.decimals)
-              .toString(16);
-            value = value.split('.')[0];
-      
-            this.secondCoinAmount = this.biswapServ.getAmountOut(amount, this.firstToken.decimals, this.secondToken.decimals, reserve1, reserve2);
+        if (isFirst && this.firstCoinAmount) {
+          var amount: number = this.firstCoinAmount;
+          this.insufficientFund = (Number(amount) > Number(this.firstCoinBalance));
+          var reserve1: BigNumber = this.firstTokenReserve;
+          var reserve2: BigNumber = this.secondTokenReserve;
+          let value = '0x' + new BigNumber(amount)
+            .shiftedBy(this.firstToken.decimals)
+            .toString(16);
+          value = value.split('.')[0];
 
-            this.liquidityPrividerFee = new BigNumber(amount).multipliedBy(new BigNumber(0.003)).toNumber();
-            this.liquidityPrividerFeeCoin = this.firstToken.symbol;
-            this.maximumSold = 0;
-            this.minimumReceived = new BigNumber(this.secondCoinAmount).multipliedBy(new BigNumber(1-this.slippage * 0.01)).toNumber();            
-          } else 
-          if(!isFirst && this.secondCoinAmount)
-          {
+          this.secondCoinAmount = this.biswapServ.getAmountOut(amount, this.firstToken.decimals, this.secondToken.decimals, reserve1, reserve2);
+
+          this.liquidityPrividerFee = new BigNumber(amount).multipliedBy(new BigNumber(0.003)).toNumber();
+          this.liquidityPrividerFeeCoin = this.firstToken.symbol;
+          this.maximumSold = 0;
+          this.minimumReceived = new BigNumber(this.secondCoinAmount).multipliedBy(new BigNumber(1 - this.slippage * 0.01)).toNumber();
+        } else
+          if (!isFirst && this.secondCoinAmount) {
             var amount: number = this.secondCoinAmount;
             this.insufficientFund = (Number(this.firstCoinAmount) > Number(this.firstCoinBalance));
             var reserve1: BigNumber = this.firstTokenReserve;
@@ -352,25 +415,25 @@ export class SwapComponent implements OnInit {
             this.liquidityPrividerFee = new BigNumber(amount).multipliedBy(new BigNumber(0.003)).toNumber();
             this.liquidityPrividerFeeCoin = this.secondToken.symbol;
             this.minimumReceived = 0;
-            this.maximumSold = new BigNumber(this.firstCoinAmount).multipliedBy(new BigNumber(1+this.slippage * 0.01)).toNumber();
+            this.maximumSold = new BigNumber(this.firstCoinAmount).multipliedBy(new BigNumber(1 + this.slippage * 0.01)).toNumber();
           }
-      
-          if((isFirst && this.firstCoinAmount) || (!isFirst && this.secondCoinAmount)) {
-            var perAmount = (this.firstCoinAmount / this.secondCoinAmount);
-      
-            this.perAmountLabel =
-              this.firstToken.symbol +
-              ' per ' +
-              this.secondToken.symbol;
-        
-            this.perAmount = perAmount.toString();   
-  
-            const currentPerAmount = this.firstTokenReserve.dividedBy(this.secondTokenReserve).toNumber();
-            const diff = perAmount > currentPerAmount ? (perAmount - currentPerAmount) : (currentPerAmount - perAmount);
-            this.priceImpact = Number((diff / perAmount * 100).toFixed(2));
-  
-            this.route = [this.firstToken.symbol, this.secondToken.symbol];
-          }
+
+        if ((isFirst && this.firstCoinAmount) || (!isFirst && this.secondCoinAmount)) {
+          var perAmount = (this.firstCoinAmount / this.secondCoinAmount);
+
+          this.perAmountLabel =
+            this.firstToken.symbol +
+            ' per ' +
+            this.secondToken.symbol;
+
+          this.perAmount = perAmount.toString();
+
+          const currentPerAmount = this.firstTokenReserve.dividedBy(this.secondTokenReserve).toNumber();
+          const diff = perAmount > currentPerAmount ? (perAmount - currentPerAmount) : (currentPerAmount - perAmount);
+          this.priceImpact = Number((diff / perAmount * 100).toFixed(2));
+
+          this.route = [this.firstToken.symbol, this.secondToken.symbol];
+        }
 
       });
 
@@ -396,16 +459,16 @@ export class SwapComponent implements OnInit {
   }
 
   refresh() {
-    if(this.account) {
-      if(this.firstToken && this.firstToken.id) {
+    if (this.account) {
+      if (this.firstToken && this.firstToken.id) {
         this.kanbanService.getTokenBalance(this.account, this.firstToken.id).subscribe(
           (balance: any) => {
             this.firstCoinBalance = balance;
           }
         );
       }
-  
-      if(this.secondToken && this.secondToken.id) {
+
+      if (this.secondToken && this.secondToken.id) {
         this.kanbanService.getTokenBalance(this.account, this.secondToken.id).subscribe(
           (balance: any) => {
             this.secondCoinBalance = balance;
@@ -418,12 +481,12 @@ export class SwapComponent implements OnInit {
   }
 
   async swapFunction() {
-    if(
+    if (
       (this.isFistToken && !this.firstCoinAmount && !this.firstCoinBalance && (this.firstCoinAmount > this.firstCoinBalance)) ||
       (!this.isFistToken && !this.secondCoinAmount && !this.secondCoinBalance && (this.secondCoinAmount > this.secondCoinBalance))) {
-        this.error = 'Not enough balance';
-        return;
-      }
+      this.error = 'Not enough balance';
+      return;
+    }
     var to = this.account;
     var timestamp = new TimestampModel(
       this.deadline,
@@ -433,18 +496,18 @@ export class SwapComponent implements OnInit {
     );
     var deadline = this.utilService.getTimestamp(timestamp);
 
-    
+
 
     let abiHex = '';
     let approveAmount;
-    if(this.isFistToken) {
+    if (this.isFistToken) {
       var path = [this.firstToken.id, this.secondToken.id];
       const amountIn = '0x' + new BigNumber(this.firstCoinAmount)
-      .shiftedBy(this.firstToken.decimals)
-      .toString(16).split('.')[0];
-      const amountOutMin = '0x' + new BigNumber(this.secondCoinAmount).multipliedBy(new BigNumber(1-this.slippage * 0.01))
-      .shiftedBy(this.secondToken.decimals)
-      .toString(16).split('.')[0];
+        .shiftedBy(this.firstToken.decimals)
+        .toString(16).split('.')[0];
+      const amountOutMin = '0x' + new BigNumber(this.secondCoinAmount).multipliedBy(new BigNumber(1 - this.slippage * 0.01))
+        .shiftedBy(this.secondToken.decimals)
+        .toString(16).split('.')[0];
       const params = [amountIn, amountOutMin, path, to, deadline];
       console.log('params is1:', params);
       abiHex = this.web3Service.swapExactTokensForTokens(params);
@@ -452,11 +515,11 @@ export class SwapComponent implements OnInit {
     } else {
       var path = [this.firstToken.id, this.secondToken.id];
       const amountOut = '0x' + new BigNumber(this.secondCoinAmount)
-      .shiftedBy(this.secondToken.decimals)
-      .toString(16).split('.')[0];
-      const amountInMax = '0x' + new BigNumber(this.firstCoinAmount).multipliedBy(new BigNumber(1+this.slippage * 0.01))
-      .shiftedBy(this.firstToken.decimals)
-      .toString(16).split('.')[0];
+        .shiftedBy(this.secondToken.decimals)
+        .toString(16).split('.')[0];
+      const amountInMax = '0x' + new BigNumber(this.firstCoinAmount).multipliedBy(new BigNumber(1 + this.slippage * 0.01))
+        .shiftedBy(this.firstToken.decimals)
+        .toString(16).split('.')[0];
       const params = [amountOut, amountInMax, path, to, deadline];
       console.log('params is2:', params);
       abiHex = this.web3Service.swapTokensForExactTokens(params);
@@ -475,7 +538,7 @@ export class SwapComponent implements OnInit {
     ];
     const alertDialogRef = this.dialog.open(AlertComponent, {
       width: '250px',
-      data: {text: 'Please approve your request in your wallet'},
+      data: { text: 'Please approve your request in your wallet' },
     });
 
     this.kanbanService
@@ -485,7 +548,7 @@ export class SwapComponent implements OnInit {
         const baseUrl = environment.production ? 'https://www.exchangily.com' : 'https://test.exchangily.com';
         //this.txHash = baseUrl + '/explorer/tx-detail/' + data;
 
-        this.txHashes = txids.map((txid: string) =>  baseUrl + '/explorer/tx-detail/' + txid);
+        this.txHashes = txids.map((txid: string) => baseUrl + '/explorer/tx-detail/' + txid);
       }).catch(
         (error: any) => {
           alertDialogRef.close();
