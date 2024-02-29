59030cf5fc782b091d6bc32a9dcaf0fca7d558fc
Ken <kenin.qiu@gmail.com>
2024-02-29 10:00:53 -0500

    f

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -18,7 +18,7 @@ export const environment = {
     kanban: 'https://kanbantest.fabcoinapi.com/',
     oldexplorerapi: 'https://testapi.fundark.com/api',
     // explorerapi: 'https://testapi.fundark.com/api',
-    explorerapi: 'https://api.pay.cool/api',
+    explorerapi: 'https://testapi.fundark.com/api',
     BTC: {
       exchangily: 'https://btctest.fabcoinapi.com/',
     },
