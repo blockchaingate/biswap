1dac99169628264ea21649c2ca2f6d96bd3ef49d
Paul Liu <paullby@gmail.com>
2024-10-07 13:40:33 -0400

    update

--- src/app/modules/swap/swap.component.ts
+++ src/app/modules/swap/swap.component.ts
@@ -632,18 +632,18 @@ export class SwapComponent implements OnInit, AfterViewInit {
   }
 
   keyUp() {
-    if(this.slippage<0 || this.slippage>100){
+    if (this.slippage < 0 || this.slippage > 100) {
       this.slippageErr = true;
     } else {
       this.slippageErr = false;
     }
   }
 
-  onBlur(){
-    if(this.slippage<0 || this.slippage>100){
+  onBlur() {
+    if (this.slippage < 0 || this.slippage > 100) {
       this.slippage = 1;
       this.slippageErr = false;
-    } 
+    }
   }
 }
 
